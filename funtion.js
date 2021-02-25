a5ca50b18904ab31ae6364c77e14f380c93c289c
asmedina24 <alejandramedina1215@gmail.com>
2021-02-25 11:08:41 -0300

    Funcion  par leer archivos .md

--- funtion.js
+++ funtion.js
@@ -1,8 +1,29 @@
-let fs = require('fs');
-let path = require('path');
+// let fs = require('fs');
+// let path = require('path');
 
-// Buscar en directorio
 
-module.exports = (directory, ext, callback) => {
 
-};
+// // Buscar en directorio
+
+// module.exports =  (directory, ext, callback) => {
+//     fs.readdir(directory, function(err, list) {
+//         if (err) {
+//             console.log(111, err)
+//             return callback(err);
+//         }
+
+//         list = list.filter((file) => {
+//             if (path.extname(file) === '.' + ext){
+//                 return true
+//             }
+//             // console.log('no hay archivo')
+//         })
+//         return callback(null, list)
+
+//     })
+    
+    
+
+// };
+
+
--- index.js
+++ index.js
@@ -1,31 +1,52 @@
 // aca busca los archivos de acuerdo a la extensión 
-let fs = require('fs');
-let path = require('path');
-let directory = process.argv[2];
-let ext = process.argv[3];
+let fs = require('fs'); // Modulo de sistema de archivos
+let path = require('path'); // coloca la ruta a absoluta, busca la ruta rela o abso
+const folder = process.argv[2]; // argumento que busca la posicion
+const allowedExt = /(.md|.mdtxt|.mdwn|.mkd|.mdown|.markdown|.mdtxt|.text|.Rmd)$/i; // Extensiones permitidas
 
-let extn = '.' + ext;
-fs.readdir(directory, (err, data)=>{
-    if(err){
-       console.log(err);
-    } else {
-       if(extn === ext){
-        let filter = [];
-        data.forEach((file)=>{
-          
-            if(path.extname(file)===extn){
+// const marked = require('marked');
+// let findMd = require('./funtion.js');
 
-                filter.push(file)
-                console.log(filter)
-                        
+const directory = (ejemplo) => {
+    fs.readdir(ejemplo, (err, data) => {
+        if (err) {
+            console.log('Directorio no encontrado');
+            console.log(err)
+        } else {
+            filter = [];
+            data.forEach(file => {
+                if (allowedExt.exec(path.extname(file))) { // Metodo exec() ejecuta busqueda sobre coincidencias, devuelve resultado como array o null 
+                    filter.push(file);  //path.extname devuelde del punto en adelante de la extension
+                }
+            });
+            console.log(filter)
+            if (filter.length === 0) { // Object.entries verifica si esta array vacio
+                console.log('Nose se encontraron archivos con extension md');
+            } else {
+                filter.forEach(fileMarkdown => {
+                    const probando = `${ejemplo}\\${fileMarkdown}`;
+                    readAr(probando)
+                })
             }
-                 
-        });
-       }
-       console.log('probando')
-       
-      
-        
-    }
-     
-});
+        }
+    });
+}
+directory(folder);
+
+// leyendo las lineas del archivos 
+const readAr = (leerArchivo) => {
+    fs.readFile(leerArchivo, 'utf-8', (error, data) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+        const lines = data.split('\n').length - 1; // 
+        console.log('Este documento tiene:', lines, 'lineas');
+
+        // console.log(data);
+
+    })
+
+
+}
+
